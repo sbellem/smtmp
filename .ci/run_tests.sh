8834cbde696bd9427d60ccd0abfe33f52f536341
Sylvain Bellemare <sbellem@gmail.com>
2019-11-11 14:27:27 -0500

    Try

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -ev
+
+cp -R Auto-Test-Data/Cert-Store/* Cert-Store/
+
+for i in $(seq 0 25); do
+  \rm -f Scripts/logs/$i
+done
+
 cp -R Auto-Test-Data/$TEST_SET/* Data/
 echo Running testscript on set $TEST_SET
 ./Scripts/test.sh
