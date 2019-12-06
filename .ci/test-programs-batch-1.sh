f3949e26dbfc911a54670a3ed3a209d1e5e7a50f
Sylvain Bellemare <sbellem@gmail.com>
2019-12-06 17:49:59 -0500

    Clean up

--- .ci/test-programs-batch-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-IMG=$DOCKER_USERNAME/scale-mamba:$COMMIT
-programs=(test_array test_branch test_branching test_comparison)
-
-docker run --rm --env TEST_SET $IMG sh .ci/run_tests.sh "${programs[@]}"
--- .ci/test-programs.sh
+++ .ci/test-programs.sh
@@ -3,7 +3,6 @@
 set -ev
 
 IMG=$DOCKER_USERNAME/scale-mamba:$COMMIT
-#programs=(test_array test_branch test_branching test_comparison)
 
 prgs_arr=($PROGRAMS)
 docker run --rm --env TEST_SET $IMG sh .ci/run_tests.sh "${prgs_arr[@]}"
