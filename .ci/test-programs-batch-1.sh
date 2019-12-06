9a9e747c548c6b427c2bedac78bd19eb90fc2520
Sylvain Bellemare <sbellem@gmail.com>
2019-12-05 21:28:23 -0500

    wip -- test batches on travis

---
+++ .ci/test-programs-batch-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ev
+
+IMG=$DOCKER_USERNAME/scale-mamba:$COMMIT
+prgrams=(test_array test_branch test_branching test_comparison)
+
+docker run --rm --env TEST_SET $IMG sh .ci/run_tests.sh "${programs[@]}"
