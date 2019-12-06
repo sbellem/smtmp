5323414967fd5ad31b09c6ac636ddc04060bd696
Sylvain Bellemare <sbellem@gmail.com>
2019-12-05 21:30:18 -0500

    fix

--- .ci/test-programs-batch-1.sh
+++ .ci/test-programs-batch-1.sh
@@ -3,6 +3,6 @@
 set -ev
 
 IMG=$DOCKER_USERNAME/scale-mamba:$COMMIT
-prgrams=(test_array test_branch test_branching test_comparison)
+programs=(test_array test_branch test_branching test_comparison)
 
 docker run --rm --env TEST_SET $IMG sh .ci/run_tests.sh "${programs[@]}"
